e98e89040045eac73787ba1abab3755955a6c1f9
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2022-05-03 00:35:32 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -8,33 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Assignment, present score value in message alert. Firs we need property to store score data.
+    @State private var score = 0
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigera", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var showingScore = false
+    @State private var scoretitle = ""
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
     ZStack {
-        Color.blue
+        LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        //Color("BackgroundColor")
             .ignoresSafeArea()
-      VStack(spacing: 30) {
+        
         VStack {
-            Text("Tap the flag")
-                .foregroundColor(Color.white)
-            Text(countries[correctAnswer])
-                .foregroundColor(Color.white)
+            Text("Guess The Flag")
+                .font(.largeTitle.bold())
+                .foregroundColor(.white)
+            VStack(spacing: 30) {
+            VStack {
+                Text("Tap the flag")
+                    .foregroundColor(Color.white)
+                    .font(.subheadline.weight(.heavy))
+                Text(countries[correctAnswer])
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle.weight(.semibold))
+            }
+            ForEach(0..<3) { number in
+                Button {
+                    // Flagg was tapped
+                    flagTapped(number)
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                        .clipShape(Capsule())
+                    //MARK: Shadow adjustment
+                        .shadow(color: .black.opacity(0.7), radius: 10, x: 10, y: 10)
+                    }
+                 }
+                Spacer()
+            }
+            Text("Soce is \(score)")
+                .font(.largeTitle.bold())
+                .foregroundColor(.white)
+            
         }
-        ForEach(0..<3) { number in
-            Button {
-                // Flagg was tapped
-            } label: {
-                Image(countries[number])
-                    .renderingMode(.original)
-                }
-             }
-          }
-       }
+        .padding()
     }
+    .alert(scoretitle, isPresented: $showingScore) {
+        Button("Continue", action: askQuestion)
+    } message: {
+        Text("Your score is \(score)")
+    }
+    }
+    //MARK: Assignment: Second we need to present score data
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoretitle = "Correct!"
+            score += 1
+        } else {
+            scoretitle = "Wrong answer!"
+            score -= 1
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
